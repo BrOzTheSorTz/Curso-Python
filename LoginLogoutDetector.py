e7aefd3ec451a132199a2091618a1f295555bf24
Jose Pablo <sortzweb@gmail.com>
2022-08-02 17:11:12 +0200

    Detector of logs

---
+++ LoginLogoutDetector.py
@@ -0,0 +1,60 @@
+
+from platform import machine
+
+
+class Event():
+    def __init__ (self,event_date, event_type,machine_name, user):
+        self.date = event_date
+        self.type = event_type
+        self.machine = machine_name
+        self.user = user
+
+
+def get_event_date(event):
+    return event.date
+
+def current_users(events):
+    #We say we want to sort it by date to have
+    # first of all the login and then the logout
+    events.sort(key=get_event_date)
+    #Now, we create a dictionary to keep the machines
+    #as keys and the people use them as values
+    machines ={}
+
+    for event in events:
+        if event.machine not in machines:
+            machines[event.machine]=set()
+        if event.type == "login":
+            machines[event.machine].add(event.user)
+        if event.type == "logout":
+
+            if event.user not in machines[event.machine]:
+                #Because it is possible that there is
+                #a person that it is not logged in
+                pass
+            else:
+                machines[event.machine].remove(event.user)
+        
+    return machines
+
+def generate_report(machines):
+    for machine, users in machines.items():
+        #We check if there have been users in the machine
+        if len(users)>0:
+            user_list = ",".join(users)
+            print("{} :{}".format(machine, user_list))
+
+
+events = [
+    Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+    Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+    Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+    Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
+    Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
+    Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
+]
+
+users = current_users(events)
+print(users)
+
+generate_report(users)
